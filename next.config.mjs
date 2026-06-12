4111b8e5e4446327de7f02b0a95d05ee67e02b85
not-ayan <notayan69@gmail.com>
2026-06-12 23:02:52 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-let userConfig = undefined
-
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -17,6 +10,7 @@ const nextConfig = {
   },
 
   images: {
+    unoptimized: true,
     domains: [
       'raw.githubusercontent.com',
       'lh3.googleusercontent.com',
@@ -98,28 +92,6 @@ const nextConfig = {
 
   poweredByHeader: false,
   compress: true,
-}
-
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
 }
 
 export default nextConfig
